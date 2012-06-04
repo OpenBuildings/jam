f1893726cd8d3bd6568cb0a7a48cfc5820253dde
Haralan Dobrev <hdobrev@despark.com>
2012-06-04 12:47:08 +0300

    all foreign keys from bleongsto associations default to null and related

--- classes/kohana/jam/association/belongsto.php
+++ classes/kohana/jam/association/belongsto.php
@@ -28,9 +28,26 @@ abstract class Kohana_Jam_Association_BelongsTo extends Jam_Association {
 	 * Default value of the column in the database
 	 * @var integer
 	 */
-	public $default = 0;
+	public $default = NULL;
+
+	/**
+	 * Should we allow NULL values for the foreign key
+	 * @var boolean
+	 */
+	public $allow_null = TRUE;
+
+	/**
+	 * Whether empty values for the foreign key should be converted to NULL
+	 * @var boolean
+	 */
+	public $convert_empty = TRUE;
 	
+	/**
+	 * Indicates whether the foreign model should be touched (updated) after the association has been updated.
+	 * @var boolean
+	 */
 	public $touch = FALSE;
+
 	/**
 	 * Automatically sets foreign to sensible defaults.
 	 *
@@ -71,7 +88,11 @@ abstract class Kohana_Jam_Association_BelongsTo extends Jam_Association {
 		/**
 		 * Assign the accual field in the database. Default value can be set with $this->default
 		 */
-		$meta->field($this->column, Jam::field('integer', array("default" => $this->default)));
+		$meta->field($this->column, Jam::field('integer', array(
+			'default' => $this->default,
+			'allow_null' => $this->allow_null,
+			'convert_empty' => $this->convert_empty
+		)));
 
 		// We initialize a bit earlier as we want to modify the $fthis->oreign array
 		parent::initialize($meta, $model, $name);
