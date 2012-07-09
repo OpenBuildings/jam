f7e4e74c438078c239b514105aa9c02aa66866ee
Ivan Kerin <ivan@openbuildings.com>
2012-07-09 12:32:16 +0300

    fix jam

--- classes/kohana/jam/builder.php
+++ classes/kohana/jam/builder.php
@@ -758,7 +758,11 @@ abstract class Kohana_Jam_Builder extends Database_Query_Builder_Select {
 			}
 			$association = $meta->association_insist($name);
 			$association->join($this, $name_alias, $type);
-			$meta = Jam::meta($association->is_polymorphic() ? $name_alias : $association->foreign());
+			$meta = Jam::meta($association->foreign());
+			if ( ! $meta AND $association->is_polymorphic())
+			{
+				$meta = Jam::meta($name_alias);
+			}
 		}
 		return $this;
 	}
