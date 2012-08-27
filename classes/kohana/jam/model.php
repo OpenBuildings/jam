72001edcd094ca319bd0fd889c8d00d4d7ed8a1a
Ivan Kerin <ivan@openbuildings.com>
2012-08-27 13:08:51 +0300

    Jam_validated is a jam model without a database

--- classes/kohana/jam/model.php
+++ classes/kohana/jam/model.php
@@ -13,27 +13,7 @@
  * @copyright  (c) 2010-2011 Jonathan Geiger
  * @license    http://www.opensource.org/licenses/isc-license.txt
  */
-abstract class Kohana_Jam_Model extends Model {
-
-	/**
-	 * @var  array  The original data set on the object
-	 */
-	protected $_original = array();
-
-	/**
-	 * @var  array  Data that's changed since the object was loaded
-	 */
-	protected $_changed = array();
-
-	/**
-	 * @var  array  Data that's already been retrieved is cached
-	 */
-	protected $_retrieved = array();
-
-	/**
-	 * @var  array  Unmapped data that is still accessible
-	 */
-	protected $_unmapped = array();
+abstract class Kohana_Jam_Model extends Jam_Validated {
 
 	/**
 	 * @var  boolean  Whether or not the model is loaded
@@ -50,21 +30,6 @@ abstract class Kohana_Jam_Model extends Model {
 	 */
 	protected $_is_saving = FALSE;
 	
-	/**
-	 * @var  boolean  Whether or not the model is validating
-	 */
-	protected $_is_validating = FALSE;
-
-	/**
-	 * @var  Jam_Meta  A copy of this object's meta object
-	 */
-	protected $_meta = NULL;
-
-	/**
-	 * @var  Boolean  A flag that keeps track of whether or not the model is valid
-	 */
-	 protected $_errors = FALSE;
-
 	/**
 	 * @var  Boolean  A flag that indicates a record has been deleted from the database
 	 */
@@ -89,9 +54,8 @@ abstract class Kohana_Jam_Model extends Model {
 		{
 			$meta_name = $this;
 		}
-
-		// Load the object's meta data for quick access
-		$this->_meta = Jam::meta($meta_name);
+		
+		parent::__construct($key, $meta_name);
 
 		$this->_meta->trigger_behavior_events($this, 'before_construct');
 
@@ -116,236 +80,6 @@ abstract class Kohana_Jam_Model extends Model {
 	}
 
 	/**
-	 * Gets the value of a field.
-	 *
-	 * Unlike Jam_Model::get(), values that are returned are cached
-	 * until they are changed and relationships are automatically select()ed.
-	 *
-	 * @see     get()
-	 * @param   string  $name The name or alias of the field you're retrieving
-	 * @return  mixed
-	 */
-	public function &__get($name)
-	{
-		if ( ! array_key_exists($name, $this->_retrieved))
-		{
-			$this->_retrieved[$name] = $this->get($name);
-		}
-
-		return $this->_retrieved[$name];
-	}
-
-	/**
-	 * Sets the value of a field.
-	 *
-	 * @see     set()
-	 * @param   string  $name  The name of the field you're setting
-	 * @param   mixed   $value The value you're setting
-	 * @return  void
-	 */
-	public function __set($name, $value)
-	{
-		// Being set by mysql_fetch_object, store the values for the constructor
-		if (empty($this->_original))
-		{
-			$this->_preload_data[$name] = $value;
-			return;
-		}
-
-		$this->set($name, $value);
-	}
-
-	/**
-	 * Add methods for this model on the fly (mixins) you can assign:
-	 * Class - loads all static methods
-	 * array or string/array callback
-	 * array of closures
-	 * @param  array|string   $callbacks 
-	 * @param  mixed $callback  
-	 * @return Jam_Meta              $this
-	 */
-	public function extend($callbacks, $callback = NULL)
-	{
-		// Handle input with second argument, so you can pass single items without an array
-		if ($callback !== NULL)
-		{
-			$callbacks = array($callbacks => $callback);
-		}
-
-		$this->_meta->events()->bind_callbacks('model', $callbacks);
-		return $this;
-	}
-
-	/**
-	 * Passes unknown methods along to the behaviors.
-	 *
-	 * @param   string  $method
-	 * @param   array   $args
-	 * @return  mixed
-	 **/
-	public function __call($method, $args)
-	{
-		return $this->_meta->events()->trigger_callback('model', $this, $method, $args);
-	}
-
-	/**
-	 * Returns true if $name is a field of the model or an unmapped column.
-	 *
-	 * This does not conform to the standard of returning FALSE if the
-	 * property is set but the value is NULL. Rather this acts more like
-	 * property_exists.
-	 *
-	 * @param  string    $name
-	 * @return  boolean
-	 */
-	public function __isset($name)
-	{
-		return (bool) ($this->_meta->attribute($name) OR array_key_exists($name, $this->_unmapped));
-	}
-
-	/**
-	 * This doesn't unset fields. Rather, it sets them to their original
-	 * value. Unmapped, changed, and retrieved values are unset.
-	 *
-	 * In essence, unsetting a field sets it as if you never made any changes
-	 * to it, and clears the cache if the value has been retrieved with those changes.
-	 *
-	 * @param   string  $name
-	 * @return  void
-	 */
-	public function __unset($name)
-	{
-		if ($this->_meta->attribute($name))
-		{
-			// Ensure changed and retrieved data is cleared
-			// This effectively clears the cache and any changes
-			unset($this->_changed[$name]);
-			unset($this->_retrieved[$name]);
-		}
-
-		// We can safely delete this no matter what
-		unset($this->_unmapped[$name]);
-	}
-
-	/**
-	 * Returns a string representation of the model in the
-	 * form of `Model_Name (id)` or `Model_Name (NULL)` if
-	 * the model is not loaded.
-	 *
-	 * This is designed to be useful for debugging.
-	 *
-	 * @return  string
-	 */
-	public function __toString()
-	{
-		return (string) get_class($this).'('.($this->id() ? $this->id() : 'NULL').')';
-	}
-
-	/**
-	 * Gets the value for a field.
-	 *
-	 * @param   string       $name The field's name
-	 * @return  array|mixed
-	 */
-	public function get($name)
-	{
-		if ($association = $this->_meta->association($name))
-		{
-			$name = $association->name;
-
-			if (array_key_exists($name, $this->_changed))
-			{
-				return $this->_changed[$name];
-			}
-			else
-			{
-				return $association->get($this, NULL, FALSE);
-			}
-		}
-		elseif ($field = $this->_meta->field($name))
-		{
-			// Alias the name to its actual name
-			$name = $field->name;
-
-			if (array_key_exists($name, $this->_changed))
-			{
-				return $field->get($this, $this->_changed[$name], TRUE);
-			}
-			else
-			{
-				return $this->original($name);
-			}
-		}
-		// Return unmapped data from custom queries
-		elseif (isset($this->_unmapped[$name]))
-		{
-			return $this->_unmapped[$name];
-		}
-	}
-
-	/**
-	 * Returns the original value of a field, before it was changed.
-	 *
-	 * This method—combined with get(), which first searches for changed
-	 * values—is useful for comparing changes that occurred on a model.
-	 *
-	 * @param   string  $field The field's or alias name
-	 * @return  mixed
-	 */
-	public function original($field_name = NULL)
-	{
-		if ($field_name === NULL)
-			return $this->_original;
-
-		if ($field = $this->_meta->field($field_name))
-		{
-			return $field->get($this, $this->_original[$field_name], FALSE);
-		}
-	}
-
-	/**
-	 * Returns an array of values in the fields.
-	 *
-	 * You can pass an array of field names to retrieve
-	 * only the values for those fields:
-	 *
-	 *     $model->as_array(array('id', 'name', 'status'));
-	 *
-	 * @param   array  $fields
-	 * @return  array
-	 */
-	public function as_array(array $fields = NULL)
-	{
-		$fields = $fields ? $fields : array_keys($this->_meta->fields());
-		$result = array();
-
-		foreach ($fields as $field)
-		{
-			$result[$field] = $this->__get($field);
-		}
-
-		return $result;
-	}
-
-	/**
-	 * Set preloaded values, without changing the save, loaded and changed flags
-	 * @param  array|string $values 
-	 * @param  mixed $value  
-	 * @return Jam_Model         $this
-	 */
-	public function retrieved($values, $value = NULL)
-	{
-		// Accept retrieved('name', 'value');
-		if ( ! is_array($values))
-		{
-			$values = array($values => $value);
-		}
-
-		$this->_retrieved = Arr::merge($this->_retrieved, $values);
-
-		return $this;
-	}
-	/**
 	 * Sets the value of a field.
 	 *
 	 * You can pass an array of key => value pairs
@@ -363,48 +97,15 @@ abstract class Kohana_Jam_Model extends Model {
 	 */
 	public function set($values, $value = NULL)
 	{
-		// Accept set('name', 'value');
-		if ( ! is_array($values))
+		parent::set($values, $value);
+
+		if ($this->_saved AND $this->changed())
 		{
-			$values = array($values => $value);
-		}
-
-		foreach ($values as $key => $value)
-		{
-			if ($field = $this->_meta->association($key))
-			{
-				$this->_changed[$field->name] = $field->set($this, $value, TRUE);
-				$this->_saved = FALSE;
-			}
-			else
-			{
-				$field = $this->_meta->field($key);
-
-				// If this isn't a field, we just throw it in unmapped
-				if ( ! $field)
-				{
-					unset($this->_retrieved[$key]);
-					$this->_unmapped[$key] = $value;
-
-					continue;
-				}
-
-				$this->_changed[$field->name] = $field->set($this, $value, TRUE);
-			}
-
-			// Invalidate the cache
-			if (array_key_exists($field->name, $this->_retrieved))
-			{
-				unset($this->_retrieved[$field->name]);
-			}
-
-			// Model is no longer saved or valid
 			$this->_saved = FALSE;
 		}
 
 		return $this;
 	}
-
 	
 	/**
 	 * Clears the object and loads an array of values into the object.
@@ -439,11 +140,6 @@ abstract class Kohana_Jam_Model extends Model {
 		return $this;
 	}
 
-	public function is_valid()
-	{
-		return count($this->errors()) == 0;
-	}
-
 	/**
 	 * Validates the current model's data
 	 *
@@ -451,60 +147,9 @@ abstract class Kohana_Jam_Model extends Model {
 	 * @param   Validation|null   $extra_validation
 	 * @return  Kohana_Jam_Model
 	 */
-	public function check()
+	public function check($force = FALSE)
 	{
-		$this->_is_validating = TRUE;
-
-		// Run validation only when new or changed
-		if ( ! $this->loaded() OR $this->changed())
-		{
-			// $this->_move_retrieved_to_changed();
-			
-			$this->_meta->trigger_attribute_events($this, 'before_check');
-
-			$this->_meta->trigger_behavior_events($this, 'before_check');
-
-
-			$this->_meta->execute_validators($this);
-
-
-			$this->_meta->trigger_attribute_events($this, 'after_check');
-
-			$this->_meta->trigger_behavior_events($this, 'after_check');
-		}
-
-		$this->_is_validating = FALSE;
-
-		return $this->is_valid();
-	}
-
-	public function check_insist()
-	{
-		if ( ! $this->check())
-			throw new Jam_Exception_Validation('There was an error validating the :model: :errors', $this);
-		
-		return $this;
-	}
-
-	/**
-	 * Get the errors from the previous check, if you provide a name, will return the errors only for that name
-	 * Automatically loads the error messages file from messages/jam-validation/<model_name>
-	 * If there are no errors yet - return NULL
-	 * 
-	 * @param  string $name the name of the field to get errors of
-	 * @return array|NULL
-	 */
-	public function errors($name = NULL)
-	{
-		if ( ! $this->_errors)
-		{
-			$this->_errors = new Jam_Errors($this, $this->_meta->errors_filename());
-		}
-
-		if ($name !== NULL)
-			return $this->_errors->messages($name);
-
-		return $this->_errors;
+		return parent::check( ! $this->loaded() OR $force);
 	}
 
 	protected function _move_retrieved_to_changed()
@@ -681,12 +326,9 @@ abstract class Kohana_Jam_Model extends Model {
 		{
 			$this->_loaded =
 			$this->_saved  = TRUE;
+			$this->_is_touched = FALSE;
 
-			$this->_errors = NULL;
-
-			$this->_changed   =
-			$this->_is_touched =
-			$this->_retrieved = array();
+			parent::revert();
 		}
 
 		return $this;
@@ -703,13 +345,7 @@ abstract class Kohana_Jam_Model extends Model {
 		$this->_is_touched =
 		$this->_saved  = FALSE;
 
-		$this->_errors = NULL;
-
-		$this->_changed   =
-		$this->_retrieved =
-		$this->_unmapped  = array();
-
-		$this->_original = $this->_meta->defaults();
+		parent::clear();
 
 		return $this;
 	}
@@ -753,16 +389,6 @@ abstract class Kohana_Jam_Model extends Model {
 	}
 
 	/**
-	 * Whether or not the model is in the process of being validated
-	 *
-	 * @return  boolean
-	 */
-	public function is_validating()
-	{
-		return ($this->_is_validating OR $this->_is_saving);
-	}
-
-	/**
 	 * Whether or not the model is deleted
 	 *
 	 * @return  boolean
@@ -770,62 +396,6 @@ abstract class Kohana_Jam_Model extends Model {
 	public function deleted()
 	{
 		return $this->_deleted;
-	}
-
-	/**
-	 * Returns whether or not the particular $field has changed.
-	 *
-	 * If $field is NULL, the method returns whether or not any
-	 * data whatsoever was changed on the model.
-	 *
-	 * @param   string   $field
-	 * @return  boolean
-	 */
-	public function changed($name = NULL)
-	{
-		if ($name)
-		{
-			return array_key_exists($name, $this->_changed);
-		}
-		else
-		{
-			return (bool) $this->_changed;
-		}
-	}
-
-	/**
-	 * Returns the value of the model's primary key
-	 *
-	 * @return  mixed
-	 */
-	public function id()
-	{
-		return $this->get($this->_meta->primary_key());
-	}
-
-	/**
-	 * Returns the value of the model's name key
-	 *
-	 * @return  mixed
-	 */
-	public function name()
-	{
-		return $this->get($this->_meta->name_key());
-	}
-
-	/**
-	 * Returns the model's meta object
-	 *
-	 * @return  Jam_Meta
-	 */
-	public function meta()
-	{
-		return $this->_meta;
-	}
-
-	public function unmapped($name)
-	{
-		return isset($this->_unmapped[$name]);
 	}
 
 	public function is_touched($value = NULL)
