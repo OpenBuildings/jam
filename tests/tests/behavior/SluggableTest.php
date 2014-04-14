c7db3228b02b33e6974ccc2d2c4e329c02e8c79c
Haralan Dobrev <hkdobrev@gmail.com>
2014-04-14 11:48:28 +0300

    Build slug before check if no primary key
    Fix #48.
    
    The slug could be built earlier if the `uses_primary_key` is set to `false`.
    
    Pros:
    - Database query is saved. We don't need the primary key to generate the slug.
    - We could perform validation on the generated slug and generally have it at
      the earliest possible time.

--- tests/tests/behavior/SluggableTest.php
+++ tests/tests/behavior/SluggableTest.php
@@ -10,21 +10,38 @@
  */
 class Jam_Behavior_SluggableTest extends Testcase_Database {
 
+    /**
+     * @covers Jam_Behavior_Sluggable::model_call_build_slug
+     * @covers Jam_Behavior_Sluggable::model_before_check
+     */
+    public function test_set_no_primary_key()
+    {
+        $tag = Jam::find('test_tag', 1);
+
+        $this->assertNotNull($tag);
+        $this->assertNotNull($tag->slug);
+
+        $tag->name = ' new tag j320&lt';
+        $tag->check();
+
+        $this->assertEquals('new-tag-j320lt', $tag->slug);
+    }
+
 	/**
 	 * @covers Jam_Behavior_Sluggable::model_call_build_slug
 	 * @covers Jam_Behavior_Sluggable::model_after_save
 	 */
-	public function test_set_no_primary_key()
+	public function test_save_with_primary_key()
 	{
-		$tag = Jam::find('test_tag', 1);
+		$video = Jam::find('test_video', 1);
 
-		$this->assertNotNull($tag);
-		$this->assertNotNull($tag->slug);
+		$this->assertNotNull($video);
+		$this->assertNotNull($video->slug);
 
-		$tag->name = ' new tag j320&lt';
-		$tag->save();
+		$video->file = ' new video j320&lt';
+		$video->save();
 
-		$this->assertEquals('new-tag-j320lt', $tag->slug);
+		$this->assertEquals('new-video-j320lt-1', $video->slug);
 	}
 
 	/**
