88e8f4db8dc724ff3b7544a720d21bf22d2def1e
Stoyan Dipchikov <sdipchikov@despark.com>
2015-12-01 11:32:46 +0200

    Fix for 0 (zero) values
    Fix for the cases when convert_empty is TRUE and you are trying to set a 0 or "0" for a value

--- classes/Kohana/Jam/Field/Integer.php
+++ classes/Kohana/Jam/Field/Integer.php
@@ -32,5 +32,36 @@ abstract class Kohana_Jam_Field_Integer extends Jam_Field {
 
 		return $value;
 	}
+	
+	/**
+	 * Like Jam_Field::_default, but it won't convert 0 and 0.0 to empty_value
+	 *
+	 * @param  Jam_Validated $model
+	 * @param  mixed $value
+	 * @return array 1st element the converted value; 2nd element a boolean
+	 * indicating if the value should not be processed further
+	 */
+	protected function _default(Jam_Validated $model, $value)
+	{
+		$return = FALSE;
+
+		$value = $this->run_filters($model, $value);
+
+		// Convert empty values to NULL, if needed
+		if ($this->convert_empty AND empty($value) AND $value !== 0 AND $value !== 0.0 AND $value !== "0" AND $value !== "0.0")
+		{
+			$value  = $this->empty_value;
+			$return = TRUE;
+		}
+
+		// Allow NULL values to pass through untouched by the field
+		if ($this->allow_null AND $value === NULL)
+		{
+			$value  = NULL;
+			$return = TRUE;
+		}
+
+		return array($value, $return);
+	}
 
 } // End Kohana_Jam_Field_Integer
