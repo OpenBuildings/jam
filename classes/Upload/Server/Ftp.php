2b696b352ede2179ee109322616daf1c4143e8d1
Haralan Dobrev <hkdobrev@gmail.com>
2018-04-04 04:36:04 +0300

    Remove obsolete left-over empty classes
    These are no longer used and they extend missing classes

--- classes/Upload/Server/Ftp.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Upload_Server_Ftp extends Kohana_Upload_Server_Ftp {}
--- classes/Upload/Server/Local.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Upload_Server_Local extends Kohana_Upload_Server_Local {}
